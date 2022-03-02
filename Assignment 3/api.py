47247c06b14246e5985b9159d2fe4fbc55e9b9d0
DEMI-Buddy <koberunnels@gmail.com>
2022-03-02 15:01:01 -0600

    Create api.py

---
+++ Assignment 3/api.py
@@ -0,0 +1,15 @@
+import requests
+username = 'Demiii'
+token = '9fff982746a18e2843567189cbaf459f2e8cfc8a'
+
+response = requests.get(
+    'https://www.pythonanywhere.com/api/v0/user/{username}/cpu/'.format(
+        username=username
+    ),
+    headers={'Authorization': 'Token {token}'.format(token=token)}
+)
+if response.status_code == 200:
+    print('CPU quota info:')
+    print(response.content)
+else:
+    print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
