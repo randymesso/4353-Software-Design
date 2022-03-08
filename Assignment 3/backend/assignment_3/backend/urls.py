11e237660597d1e833b531876b4549ff1face850
DEMI-Buddy <koberunnels@gmail.com>
2022-03-08 00:13:44 -0600

    fixed view connections

--- Assignment 3/backend/assignment_3/backend/urls.py
+++ Assignment 3/backend/assignment_3/backend/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('',views.front_page,name='front_page'),
     path('login',views.login,name='login'),
     path('register',views.register,name='register'),
-    path('profile_manager',views.register,name='profile_manager'),
-    path('fuel_history',views.register,name='fuel_history'),
-    path('fuel_quote',views.register,name='fuel_quote')
+    path('profile_manager',views.profile_manager,name='profile_manager'),
+    path('fuel_history',views.fuel_history,name='fuel_history'),
+    path('fuel_quote',views.fuel_quote,name='fuel_quote')
 ]
