261751a8263a7dc8576dbeba0dbe876d341a9967
DEMI-Buddy <koberunnels@gmail.com>
2022-05-05 19:57:07 -0500

    Finalizing

--- Assignment 5/app/web_app/admin.py
+++ Assignment 5/app/web_app/admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserCreationForm
 
 from .forms import UserCreation, UserChange
-from .models import UserCredentials,ClientInformation
+from .models import UserCredentials,ClientInformation,Fuel_Quote
 
 class AdminCredentials(UserAdmin):
     add_form = UserCreation
@@ -11,5 +11,6 @@ class AdminCredentials(UserAdmin):
     model = UserAdmin
     list_display = ["username","password",]
     
+admin.site.register(Fuel_Quote)
 admin.site.register(UserCredentials, UserAdmin)
 admin.site.register(ClientInformation)
